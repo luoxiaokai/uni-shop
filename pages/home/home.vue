717ed32daec4b2d498d0b45a010f943daa3e1f78
luoxiaokai <1052535284@qq.com>
2022-04-05 14:43:55 +0800

    修改

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,44 @@
 <template>
 	<view>
-		Home
+		<!-- 轮播图的区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item,index) in swiperList" :key="index">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+					<image :src="item.image_src" mode=""></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<!-- 分类导航分类 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item,index) in navList" :key="index" @click="navClickHandler(item)">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
+		<!-- 楼层区域 -->
+		<!-- 楼层容器 -->
+		<view class="floor-list">
+			<!-- 每一个楼层的item项 -->
+			<view class="floor-item" v-for="(item,index) in floorList" :key="index">
+				<!-- 楼层的标题 -->
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<!-- 楼层的图片区域 -->
+				<view class="floor-img-box">
+					<!-- 左侧盒子 -->
+					<navigator class="left-img-box" :url="item.product_list[0].url">
+						<image :src="item.product_list[0].image_src"
+							:style="{width:item.product_list[0].image_width+'rpx'}" mode="widthFix"></image>
+					</navigator>
+					<!-- 右侧的盒子 -->
+					<view class="right-img-box">
+						<navigator class="right-img-item" v-for="(item1,index1) in item.product_list" :key="index1"
+							v-if="index1 !== 0" :url="item1.url">
+							<image :src="item1.image_src" mode="widthFix" :style="{width:item1.image_width+'rpx'}">
+							</image>
+						</navigator>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +46,105 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图数据
+				swiperList: [],
+				// 分类导航数据
+				navList: [],
+				// 楼层数据
+				floorList: []
 			};
+		},
+		onLoad() {
+			// 调用方法 获取轮播图的数据
+			this.getswiperList()
+			this.getNavList()
+			this.getFloorList()
+		},
+		methods: {
+			// 轮播图数据
+			async getswiperList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.swiperList = res.message
+				// console.log(this.swiperList)
+			},
+			// 分类导航数据
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.navList = res.message
+				console.log(this.navList)
+			},
+			navClickHandler(item) {
+				if (item.name === "分类") {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			},
+			// 获取楼层数据
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 对数据进行处理
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+				console.log(this.floorList)
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			height: 100%;
+			width: 100%;
+		}
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		.nav-img {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	.floor-list {
+		.floor-item {
+			.floor-title {
+				width: 100%;
+				height: 60rpx;
+
+			}
+		}
+
+		.floor-img-box {
+			display: flex;
+			padding-left: 10rpx;
+		}
+
+		.right-img-box {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-around;
+		}
+	}
 </style>
